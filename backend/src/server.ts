63aa3e577bbca60e464e080ec910e5f1bdb860d5
Imad Ibrahim <imadibrahim164@gmail.com>
2023-11-14 19:46:23 +0530

    changed port

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -5,7 +5,8 @@ import { fetchAirtableData } from "./util/AirtablUtil.ts"
 
 // Create a new Oak application
 const app = new Application();
-const PORT = 8000;
+// const PORT = 8000;
+const PORT = Deno.env.get("PORT");
 
 // Enable CORS for all routes
 app.use(oakCors());

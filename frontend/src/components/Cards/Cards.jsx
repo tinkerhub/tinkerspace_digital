c95d7b74d80e87bc05aceeb314d4bbd5cbd942e5
Imad Ibrahim <imadibrahim164@gmail.com>
2023-11-16 20:43:18 +0530

    truncated some texts

--- frontend/src/components/Cards/Cards.jsx
+++ frontend/src/components/Cards/Cards.jsx
@@ -64,7 +64,7 @@ function Cards(props) {
             <h2 className="w-[150px] h-[24px] text-[20px] font-bold">{truncate(data.fields['First name (from Your name)'][0], 11)}</h2>
             <hr className=' border-t-[2px]' style={{borderColor: '#876100'}} />
             <p className="w-[150px] h-[22px] text-[18px]">{data.fields["Enter the domain that you will be working on."] ? truncate(data.fields["Enter the domain that you will be working on."],10) : 'Undefined' }</p>
-            <p className="w-[150px] h-[22px] text-[18px]" style={{color: '#876100'}}>{data.fields["What will you be doing at the space today?"]}</p>
+            <p className="w-[150px] h-[22px] text-[18px]" style={{color: '#876100'}}>{truncate(data.fields["What will you be doing at the space today?"],19)}</p>
           </div>
         </div>
       </div>
@@ -92,7 +92,7 @@ const renderCardMentee = (data) => {
                     <h2 className="w-[117.66px] h-[13px] text-[15.69px] font-bold">{truncate(data.fields['First name (from Your name)'][0], 11)}</h2>
                     <hr className=' border-t-[1px]' style={{borderColor: '#876100'}} />
                     <p className="w-[117.66px] h-[8px] text-[14.12px]">{data.fields["Enter the domain that you will be working on."] ? truncate(data.fields["Enter the domain that you will be working on."],10) : 'Undefined' }</p>
-                    <p className="w-[117.66px] h-[17px] text-[14.12px]" style={{color: '#876100'}}>{data.fields["What will you be doing at the space today?"]}</p>
+                    <p className="w-[117.66px] h-[17px] text-[14.12px]" style={{color: '#876100'}}>{truncate(data.fields["What will you be doing at the space today?"],19)}</p>
                 </div>  
             </div>
         </div>

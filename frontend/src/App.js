efe1edf0a26bafa16751696572a33a94445c07f7
Niyas <niyamax@gmail.com>
2025-04-29 14:14:53 +0530

    Emoji background and clean card

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,12 @@
 import React, { useEffect, useState } from 'react';
-import Navbarr from './components/Navbar/Navbar';
-import Cards from './components/Cards/Cards';
 import './fonts/fonts.css';
-import WPage from './components/WelcomePage/WPage';
-import { useTimer } from './utils/useTimer'; 
 import { fetchData } from './utils/fetchData';
-import { removeDuplicates } from './utils/removeDuplicates'
-import Lottie from 'lottie-react';
-import animationData from './components/animations/snoman.json';
-// import animationData1 from './components/animations/lottieAnim1.json';
-import animationData2 from './components/animations/lottieAnim2.json';
-import animationData3 from './components/animations/lottieAnim3.json';
-// import animationData4 from './components/animations/lottieAnim4.json';
-import animationData5 from './components/animations/lottieAnim5.json';
+import { removeDuplicates } from './utils/removeDuplicates';
+import PaginatedCardGrid from './components/PaginatedCardGrid';
+import EmojiBackground from './components/EmojiBackground';
 
 function App() {
     const [data, setData] = useState([]);
-    const [showWPage, setShowWPage] = useState(true);
 
     useEffect(() => {
         const fetchRecords = async () => {
@@ -33,50 +23,16 @@ function App() {
         return () => clearInterval(interval);
     }, []);
 
-    // Use the custom timer hook
-    useTimer(6500, () => setShowWPage(false)); // Call setShowWPage after 6500ms
-
     return (
-        <div className='tailwind-scope' style={{ fontFamily: 'ClashDisplay' }}>
-              {/* {showWPage ? <WPage /> : <> */}
-                <Navbarr count={data.length} />
-                <Cards data={data} />
-            
-             {/* Lottie animation */}
-{/* 
-            <Lottie 
-                className="absolute z-10 "
-                style={{ width: '90px', right: '130px', bottom: '10px', }}
-                animationData={animationData} 
-                loop 
-                autoplay 
-            />
-
-            <Lottie 
-                className="absolute z-10 "
-                style={{ width: '100px', left: '150px', bottom: '10px', }}
-                animationData={animationData2} 
-                loop 
-                autoplay 
-            />
-
-            <Lottie 
-                className="absolute z-10 "
-                style={{ width: '160px', left: '10px', bottom: '10px', }}
-                animationData={animationData3} 
-                loop 
-                autoplay 
-            />
-
-            <Lottie 
-                className="absolute z-10 "
-                style={{ width: '130px', right: '10px', bottom: '10px', }}
-                animationData={animationData5} 
-                loop 
-                autoplay 
-            /> */}
-            
-            {/* </>} */}
+        <div style={{ 
+            position: 'relative',
+            width: '100vw',
+            height: '100vh',
+            overflow: 'hidden',
+            fontFamily: 'ClashDisplay'
+        }}>
+            <EmojiBackground />
+            <PaginatedCardGrid data={data} />
         </div>
     );
 }
